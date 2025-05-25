7d7fb514cf169f59b0b306d78a399e699f52abe1
hhello_hr <1132874683@qq.com>
2025-05-25 22:26:44 +0800

    calcute similarity of test1 and test2

--- SC_clean_HR.py
+++ SC_clean_HR.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn.functional as F
+from torch import nn
 import cv2
 import shutil
 from torchvision import models, transforms
@@ -9,7 +10,25 @@ import heapq  # 用于获取最高和最低相似度的图像
 
 # 设置设备
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def make_models(target_layer=4):
+    resnet = models.resnet50(pretrained=True).to(device)
 
+    # 获取模型的特定层的输出
+    layers = [
+        nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1),
+        nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1, resnet.layer2),
+        nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1, resnet.layer2,
+                      resnet.layer3),
+        nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1, resnet.layer2,
+                      resnet.layer3, resnet.layer4, resnet.avgpool),
+    ]
+
+    # if target_layer < 1 or target_layer > 4:
+    #     raise ValueError(f"Invalid target layer: {target_layer}. Target layer should be between 1 and 4.")
+
+    # 截取模型到指定层
+    feature_extractor = nn.Sequential(*layers[target_layer - 1]).eval()
+    return feature_extractor
 def count_images_in_folder(folder_path):
     """统计文件夹中的图像数量"""
     image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
@@ -29,7 +48,7 @@ def save_similarity_to_txt(similarity_dict, output_path):
             f.write(f"{filename}, {similarity:.4f}\n")
     print(f"已保存排序后的相似度结果到: {output_path}")
 
-def calculate_similarities(model, transform, dataSource):
+def calculate_similarities(model,resnet50, transforms_original,transform, dataSource):
     """
     计算所有图像的相似度
     返回: 
@@ -54,14 +73,19 @@ def calculate_similarities(model, transform, dataSource):
                 img1, img2 = transform(img)
                 img1 = img1.unsqueeze(0).to(device)
                 img2 = img2.unsqueeze(0).to(device)
+                img_res = transforms_original(img).unsqueeze(0).to(device)
+                
 
                 # 提取特征
                 with torch.no_grad():
                     view1 = model(img1)
                     view2 = model(img2)
+                    res_view = resnet50(img_res).squeeze()
+                    test1 =res_view-view1
+                    test2 =res_view-view2
 
                 # 计算相似度
-                similarity = F.cosine_similarity(view1, view2).item()
+                similarity = F.cosine_similarity(test1, test2).item()
                 similarity_dict[file] = similarity
                 
                 # 维护最高和最低相似度的列表
@@ -121,19 +145,28 @@ def process_threshold(similarity_dict, dataSource, output_root, threshold):
 def clean_images_by_similarity(model_path, dataSource, output_root, thresholds):
     """主清洗函数"""
     # 加载模型
-    base_model = torch.load(model_path + 'base-best.pth', map_location=device)
+    base_model = torch.load(model_path + 'base-last.pth', map_location=device)
     base_model.eval().to(device)
+    resnet50 = make_models()
+    resnet50 = resnet50.eval().to(device)
 
     # 数据增强器
     transformT, transformT1, _ = get_byol_transforms(64, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     transform = MultiViewDataInjector([transformT, transformT1])
+    norm_mean = [0.485, 0.456, 0.406]
+    norm_std = [0.229, 0.224, 0.225]
+    transforms_original = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+        transforms.Normalize(norm_mean, norm_std),
+    ])
 
     # 清空输出目录
     shutil.rmtree(output_root, ignore_errors=True)
     os.makedirs(output_root, exist_ok=True)
 
     # 计算所有图像的相似度
-    similarity_dict, top_5, bottom_5 = calculate_similarities(base_model, transform, dataSource)
+    similarity_dict, top_5, bottom_5 = calculate_similarities(base_model,resnet50,transforms_original, transform, dataSource)
     
     # 保存相似度结果到txt文件
     similarity_txt_path = os.path.join(output_root, "similarity_results.txt")
@@ -169,12 +202,12 @@ def clean_images_by_similarity(model_path, dataSource, output_root, thresholds):
         print(f"阈值 {threshold}: 保留 {kept} 张, 删除 {deleted} 张, 保留比例 {kept/(kept+deleted):.2%}")
 
 if __name__ == '__main__':
-    dataSource = '/home/huangrui/Codes/SubStyleClassfication/merged_data/test'
-    model_path = '/home/huangrui/Codes/SubStyleClassfication/model/pandora-SSR-resnet50-2025-05-09-15-20-31-SSC-'
+    dataSource = '/home/huangrui/Codes/SubStyleClassfication/human_cleaned_data/2'
+    model_path = '/home/huangrui/Codes/SubStyleClassfication/model/pandora-SSR-resnet50-2025-05-23-18-01-31-SSC-'
     output_root = '/home/huangrui/Codes/SubStyleClassfication/uncertain_test'
     
     # 自定义阈值集合
-    thresholds = [ 0.3, 0.4]  # 可以修改这个列表
+    thresholds = [0.3, 0.4]  # 可以修改这个列表
     
     clean_images_by_similarity(model_path, dataSource, output_root, thresholds)
    
@@ -182,16 +215,17 @@ if __name__ == '__main__':
     image_count_before = count_images_in_folder(dataSource)
     print(f"\n原来文件夹中共有 {image_count_before} 张图像")
     
-    # 打印每个阈值的结果
-    for threshold in thresholds:
-        threshold_dir = os.path.join(output_root, str(threshold))
-        cleaned_dir = os.path.join(threshold_dir, "cleaned")
-        deleted_dir = os.path.join(threshold_dir, "deleted")
+    # # 打印每个阈值的结果
+    # for threshold in thresholds:
+    #     threshold_dir = os.path.join(output_root, str(threshold))
+    #     cleaned_dir = os.path.join(threshold_dir, "cleaned")
+    #     deleted_dir = os.path.join(threshold_dir, "deleted")
         
-        image_count_after = count_images_in_folder(cleaned_dir)
-        image_count_del = count_images_in_folder(deleted_dir)
+    #     image_count_after = count_images_in_folder(cleaned_dir)
+    #     image_count_del = count_images_in_folder(deleted_dir)
         
-        print(f"\n阈值 {threshold} 结果:")
-        print(f"清洗后文件夹中共有 {image_count_after} 张图像")
-        print(f"删除了共 {image_count_del} 张图像")
-        print(f"保留比例: {image_count_after/(image_count_after+image_count_del):.2%}")
+    #     print(f"\n阈值 {threshold} 结果:")
+    #     print(f"清洗后文件夹中共有 {image_count_after} 张图像")
+    #     print(f"删除了共 {image_count_del} 张图像")
+    #     print(f"保留比例: {image_count_after/(image_count_after+image_count_del):.2%}")
+
