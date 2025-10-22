b9d9152c9429c743a1c7bef7da05fd8620919cca
cuijia1247 <cuijia1247@hotmail.com>
2025-10-22 10:02:11 +0800

    vit-l-16 trainging on Painting91

--- ssc_train_transformer.py
+++ ssc_train_transformer.py
@@ -23,7 +23,7 @@ device0 = torch.device('cuda:0') if torch.cuda.is_available() else torch.device(
 device1 = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 def parameter_load():
-    epochs = 200 #best, perhaps6001
+    epochs = 120 #best, perhaps6001
     backbone = 'vit_large_patch16_224'
     ssc_backend = 'vit_large_patch16_224'
     ssc_input = 1024
@@ -32,16 +32,16 @@ def parameter_load():
     batch_size_sample = 'None'
     offset_bs = 512
     # base_lr = 0.008 # best
-    base_lr = 0.008 # current
+    base_lr = 0.0003 # current
     image_size = 64 # best
     # classfier_iteration = 180 # best
-    classfier_iteration = 180  # current
-    classifier_lr = 0.0004 #best
+    classfier_iteration = 40  # current
+    classifier_lr = 0.0001 #best
     # classifier_structure = '2048-1024-512-13 with dropout'
     # classifier_training_gap = 30 # best
     # classifier_test_gap = 30 # best
-    classifier_training_gap = 30 # current
-    classifier_test_gap = 30 # current
+    classifier_training_gap = 10 # current
+    classifier_test_gap = 8 # current
     model_name = ''
     return (epochs, batch_size_, offset_bs, base_lr, image_size, classfier_iteration, classifier_lr, model_name, batch_size_sample,
             classifier_training_gap, backbone, ssc_backend, ssc_input, ssc_output, classifier_test_gap)#, classifier_structure
@@ -122,6 +122,11 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
         last_accuracy = 0.0
         logger.info('SSC fine-tuning mode is ready...')
 
+    train_feature_path = '/home/cuijia1247/Codes/SubStyleClassfication/pretrainFeatures/Painting91_vit_train_features.pkl'
+    train_feature_dict = load_dataFeatures(train_feature_path)
+    test_feature_path = '/home/cuijia1247/Codes/SubStyleClassfication/pretrainFeatures/Painting91_vit_test_features.pkl'
+    test_feature_dict = load_dataFeatures(test_feature_path)
+
     for iteration in range(iterations):
         logger.info('The iteration is %d', iteration)
         #setup dataset & dataloader
@@ -134,14 +139,9 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
         logger.info('SSC ' + dataSource + 'for ' + str(iteration) + ' iterations is ready...')
 
-        train_feature_path = '/home/cuijia1247/Codes/SubStyleClassfication/pretrainFeatures/Painting91_vit_train_features.pkl'
-        train_feature_dict = load_dataFeatures(train_feature_path)
-        test_feature_path = '/home/cuijia1247/Codes/SubStyleClassfication/pretrainFeatures/Painting91_vit_test_features.pkl'
-        test_feature_dict = load_dataFeatures(test_feature_path)
-    
-
         for epoch in range(epochs):
             # print('epoch is {}'.format(epoch))
+            model.train()
             tk0 = trainloader
             train_loss = []
             # temploss = total_loss / (1860*100)
@@ -160,6 +160,7 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                 # print('The epoch is {}, Vic train loss is {}'.format(epoch, np.mean(train_loss)))
                 # train the style classifier every 500 iterations
             if epoch % classifier_training_gap_ == 0 and epoch != 0 or epoch == epochs-1:
+                model.eval()
                 classifier = Classifier(ssc_output_, class_number).to(device0)
                 classifier_criterion = nn.CrossEntropyLoss()
                 classifier_optimizer = torch.optim.Adam(classifier.parameters(), lr=classifier_lr_)
@@ -170,14 +171,15 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                 # correct = 0.0
                 # total_number = len(trainset)
                 for i in range(classifier_iteration_):
+                    classifier.train()
                     trainstyle_loss = []
                     total_correct = 0.0
                     tk1 = trainloader
                     tk2 = testloader
                     for view1, view2, label, names, original in tk1:
                         correct = 0.0
-                        view1 = view1.to(device0).detach()
-                        view2 = view2.to(device0).detach()
+                        view1 = view1.to(device1).detach()
+                        view2 = view2.to(device1).detach()
                         # original = original.to(device1)
                         # backbone_view = vitTransformer(original)
                         # backbone_view = model.backend(original)
@@ -185,7 +187,7 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                         for name in names:
                             feature_list.append(train_feature_dict[name])
                         # Stack features into a tensor and move to the correct device
-                        backbone_view = torch.stack(feature_list, dim=0).to(device0)
+                        backbone_view = torch.stack(feature_list, dim=0).to(device1)
                         img1 = model(view1)  # only use view 1
                         img2 = model(view2)
                         test1 = backbone_view - img1
@@ -198,7 +200,7 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                         # original_label = label
                         # label = label.cpu().float()-1
                         label = label - 1
-                        label = Variable(label).cuda()
+                        label = Variable(label).to(device1)
                         style_loss = classifier_criterion(prediction, label)
                         classifier_optimizer.zero_grad()
                         # style_loss.requires_grad_()
@@ -211,7 +213,7 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                     # total_loss += style_loss
                     trainstyle_loss.append(style_loss.item())
                     # print('The correct/total_correct--total is {}/{}--{}'.format(correct, total_correct, len(view1)))
-                    if i % 30 == 29: #################need adjust based on performance#####################
+                    if i % 10 == 9: #################need adjust based on performance#####################
                         logger.info('The classifer-train round is %d, the training accuracy is %d/%d', i, total_correct,
                                     len(trainset))
                         # print('The cla-train round is {}, the training ratio is {}/{}'.format(i, total_correct, len(trainset)))
@@ -221,16 +223,16 @@ def SSCtrain(logger, model_path, current_time, opt_model_name, dataset, class_nu
                         classifier.eval()
                         for view1, view2, label, names_, original in tk2:
                             correct_ = 0.0
-                            view1 = view1.to(device0).detach()
-                            view2 = view2.to(device0).detach()
-                            original = original.to(device1)
+                            view1 = view1.to(device1).detach()
+                            view2 = view2.to(device1).detach()
+                            # original = original.to(device1)
                             # backbone_view = vitTransformer(original)
                             # backbone_view = model.backend(original)
                             feature_list = []
                             for name_ in names_:
                                 feature_list.append(test_feature_dict[name_])
                             # Stack features into a tensor and move to the correct device
-                            backbone_view = torch.stack(feature_list, dim=0).to(device0)
+                            backbone_view = torch.stack(feature_list, dim=0).to(device1)
                             img1 = model(view1)  # only use view 1
                             img2 = model(view2)
                             test1 = backbone_view - img1
@@ -315,7 +317,7 @@ if __name__ == '__main__':
     filehandler.setFormatter(formatter)
     logger.addHandler(filehandler)
     ##########new add 20251018####################
-    iterations = 3
+    iterations = 2
     training_mode = 'original' # 'original' or 'fine-tuning'
     base_model_path = './model/ssc-painting91-SSR-resnet50-2025-10-18-10-10-10-SSC-base-best.pth' # only for the fine-tuning mode
     ##########new add 20251018####################
